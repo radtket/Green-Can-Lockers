229d01e050c6383c5bd8194b32c3bc1526d917d1
Taylor Radtke <taylorradtke@gmail.com>
2016-07-16 18:48:07 -0400

    macbook transfer

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -14,21 +14,7 @@
 	});
 
 
-      $('#my-video-display').prettyEmbed({
-		videoID: '1cwKcvOMq7I',
-		previewSize: 'hd',              // use either this option...
-
-		// Embed controls
-		showInfo: false,
-		showControls: true,
-		loop: false,
-
-		colorScheme: 'dark',
-		showRelated: false,
-
-		useFitVids: true,
-		playerInfo: false
-      });
+     $('#my-video-display').fitVids();
 
     });
     
